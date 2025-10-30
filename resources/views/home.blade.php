d3386423b5715cfb315c484f95ea5862e8fdd2ee
Jopel Simarmata <iss23036@students.del.ac.id>
2025-10-30 17:21:41 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+        <h2>Register</h2>
+        <form action="/register" method="POST">
+            @csrf
+            <input name="name" type="text" placeholder="name">
+            <input name="email" type="email" placeholder="email">
+            <input name="password"type="password" placeholder="password">
+            <button type="submit">Register</button>
+    </div>
+</body>
+</html>
